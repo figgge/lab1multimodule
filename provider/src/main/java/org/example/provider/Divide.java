4a1468dd142dba2b22568787222af20834735d87
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-04-27 13:31:39 +0200

    Add Calculations

--- provider/src/main/java/org/example/provider/Divide.java
+++ provider/src/main/java/org/example/provider/Divide.java
@@ -3,11 +3,10 @@ package org.example.provider;
 import org.example.service.Calculate;
 import org.example.service.annotation.Operation;
 
-@Operation("add")
-public class Add implements Calculate {
-
+@Operation("divide")
+public class Divide implements Calculate {
     @Override
     public double calculate(double a, double b) {
-        return a + b;
+        return a / b;
     }
 }
--- provider/src/main/java/org/example/provider/Subtract.java
+++ provider/src/main/java/org/example/provider/Subtract.java
@@ -1,7 +1,9 @@
 package org.example.provider;
 
 import org.example.service.Calculate;
+import org.example.service.annotation.Operation;
 
+@Operation("subtract")
 public class Subtract implements Calculate {
     @Override
     public double calculate(double a, double b) {
