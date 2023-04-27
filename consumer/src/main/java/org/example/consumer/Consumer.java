642a2e316891b708a0ee6bb7c25bfa20499a84cb
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-04-27 13:35:14 +0200

    Add Multiply and Divide to module-info

--- consumer/src/main/java/org/example/consumer/Consumer.java
+++ consumer/src/main/java/org/example/consumer/Consumer.java
@@ -1,20 +1,93 @@
 package org.example.consumer;
 
 import org.example.service.Calculate;
+import org.example.service.annotation.Operation;
 
-import java.util.ServiceLoader;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Consumer {
+    static Scanner scanner = new Scanner(System.in);
+    static ServiceLoader<Calculate> calculations = ServiceLoader.load(Calculate.class);
 
     public static void main(String[] args) {
 
-        Calculate calculate;
+        menu();
 
-//        System.out.println(calculate.calculate(1, 2));
-        ServiceLoader<Calculate> calculations = ServiceLoader.load(Calculate.class);
+    }
 
-        for (var calculation : calculations) {
-            System.out.println(calculation.calculate(5, 2));
+    private static Set<Calculate> getCalculations(String operation) {
+        return calculations.stream()
+                .filter(c -> c.type().isAnnotationPresent(Operation.class)
+                        && c.type().getAnnotation(Operation.class).value().equals(operation))
+                .map(ServiceLoader.Provider::get)
+                .collect(Collectors.toSet());
+    }
+
+    private static void displayMenu() {
+        System.out.print("""
+                Menu
+                ====================
+                1. Add
+                2. Subtract
+                3. Multiply
+                4. Divide
+                5. Exit
+                                        
+                Choose an operation:
+                """);
+    }
+
+    private static void menu() {
+        displayMenu();
+
+        switch (scanner.next()) {
+            case "1", "add", "+" -> add(getDouble(), getDouble());
+            case "2", "subtract", "-" -> subtract(getDouble(), getDouble());
+            case "3", "multiply", "*" -> multiply(getDouble(), getDouble());
+            case "4", "divide", "/" -> divide(getDouble(), getDouble());
+            case "5", "exit" -> System.exit(0);
+            default -> System.out.println("Invalid operation");
+        }
+        System.out.println();
+        menu();
+    }
+
+
+    private static void add(double a, double b) {
+        var additions = getCalculations("add");
+        for (var calculation : additions) {
+            System.out.println(a + " + " + b + " = " + calculation.calculate(a, b));
         }
     }
+
+    private static void subtract(double a, double b) {
+        var subtractions = getCalculations("subtract");
+        for (var calculation : subtractions) {
+            System.out.println(a + " - " + b + " = " + calculation.calculate(a, b));
+        }
+
+    }
+
+    private static void multiply(double a, double b) {
+        var multiplications = getCalculations("multiply");
+        for (var calculation : multiplications) {
+            System.out.println(a + " * " + b + " = " + calculation.calculate(a, b));
+        }
+
+    }
+
+    private static void divide(double a, double b) {
+        var divisions = getCalculations("divide");
+        for (var calculation : divisions) {
+            System.out.println(a + " / " + b + " = " + calculation.calculate(a, b));
+        }
+    }
+
+    private static double getDouble() {
+        System.out.print("Enter a number: ");
+        return scanner.nextDouble();
+    }
+
+
 }
--- provider/src/main/java/module-info.java
+++ provider/src/main/java/module-info.java
@@ -1,9 +1,11 @@
 import org.example.service.Calculate;
 import org.example.provider.Add;
 import org.example.provider.Subtract;
+import org.example.provider.Multiply;
+import org.example.provider.Divide;
 
 module org.example.provider {
     requires org.example.service;
     opens org.example.provider;
-    provides Calculate with Add, Subtract;
+    provides Calculate with Add, Subtract, Multiply, Divide;
 }
